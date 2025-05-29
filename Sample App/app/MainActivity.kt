d10d7c4090499e4c01a69981bb8400cbecb75d3e
John Louie Ubias <johnlouieubiasxd50@gmail.com>
2025-05-29 09:47:54 +0800

    Sample App
    Sample App

---
+++ Sample App/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.labactivity.LabAct4_Ubias
+
+import android.app.AppComponentFactory
+        class MainActivity : AppComponentFactory(){
+            override fun onCreate(savedInstanceState: Bundle?)
+        }
